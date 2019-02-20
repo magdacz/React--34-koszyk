540d3a89451ffeb983273458c2dc7cf571393da1
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-02-20 21:19:30 +0100

    Update

--- app.js
+++ app.js
@@ -24,12 +24,13 @@ handleBuy = () => {
 }
 
 render() {
+    const {shoppingCart, availableProducts} = this.state;
     return (
         <div>
-            <button disabled={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
-            <span> {this.state.shoppingCart} </span>
-            <button disabled={this.state.shoppingCart === this.state.availableProducts ? true : false} onClick={this.handleAddProductToCart}>+</button>
-            {this.state.shoppingCart > 0 && <button onClick={this.handleBuy}>Kup</button>}
+            <button disabled={shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
+            <span style={shoppingCart === 0 ? {opacity: 0.3} : {}}> {shoppingCart} </span>
+            <button disabled={shoppingCart === availableProducts ? true : false} onClick={this.handleAddProductToCart}>+</button>
+            {shoppingCart > 0 && <button onClick={this.handleBuy}>Kup</button>}
         </div>
     )
 }
