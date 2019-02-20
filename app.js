0eedf330d842eaffd6d49ab990e0729e29fd3893
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2019-02-20 20:59:19 +0100

    Add button

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 class App extends React.Component {
     state = {
         availableProducts: 10,
-        shoppingCart: 3,
+        shoppingCart: 0,
     }
 
 handleRemoveFromCart = () => {
@@ -16,12 +16,20 @@ handleAddProductToCart = () => {
     })
 }
 
+handleBuy = () => {
+    this.setState({
+        availableProducts: this.state.availableProducts - this.state.shoppingCart,
+        shoppingCart: 0,
+    })
+}
+
 render() {
     return (
         <div>
             <button disabled={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
             <span> {this.state.shoppingCart} </span>
             <button disabled={this.state.shoppingCart === this.state.availableProducts ? true : false} onClick={this.handleAddProductToCart}>+</button>
+            {this.state.shoppingCart > 0 && <button onClick={this.handleBuy}>Kup</button>}
         </div>
     )
 }
